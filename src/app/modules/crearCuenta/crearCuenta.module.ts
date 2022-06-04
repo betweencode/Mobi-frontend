310cbf306939096cf62ab9917498f4b549d683df
samv <mariscal@gmail.com>
2022-06-04 09:05:16 -0500

    cuenta creada

--- src/app/modules/crearCuenta/crearCuenta.module.ts
+++ src/app/modules/crearCuenta/crearCuenta.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from "@angular/core";
 import { crearCuentaRoutingModule } from "./crearCuenta-routing.module";
 import { CrearcuentaComponent } from './pages/crearcuenta/crearcuenta.component';
 import { OtpComponent } from './pages/otp/otp.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FormularioCrearcuentaComponent } from './pages/formulario-crearcuenta/formulario-crearcuenta.component';
+import { FormularioOtpComponent } from './pages/formulario-otp/formulario-otp.component';
 
 @NgModule({
     declarations:[
     CrearcuentaComponent,
-    OtpComponent
+    OtpComponent,
+    HeaderComponent,
+    FormularioCrearcuentaComponent,
+    FormularioOtpComponent
   ],
     imports:[CommonModule,crearCuentaRoutingModule]
 })
