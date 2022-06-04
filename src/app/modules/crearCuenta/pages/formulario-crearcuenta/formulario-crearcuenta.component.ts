7c32ce217f4032198eb91868ada8ba6447d2fcca
samv <mariscal@gmail.com>
2022-06-04 09:05:24 -0500

    cuenta creada

---
+++ src/app/modules/crearCuenta/pages/formulario-crearcuenta/formulario-crearcuenta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-crearcuenta',
+  templateUrl: './formulario-crearcuenta.component.html',
+  styleUrls: ['./formulario-crearcuenta.component.scss']
+})
+export class FormularioCrearcuentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/crearCuenta/pages/formulario-otp/formulario-otp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-otp',
+  templateUrl: './formulario-otp.component.html',
+  styleUrls: ['./formulario-otp.component.scss']
+})
+export class FormularioOtpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
