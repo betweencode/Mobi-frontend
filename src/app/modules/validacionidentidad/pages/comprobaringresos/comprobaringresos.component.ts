2a51d8a0d43a33a4de249a9e6c97dca0f9b8ca82
samv <mariscal@gmail.com>
2022-06-06 09:55:56 -0500

    final

---
+++ src/app/modules/validacionidentidad/pages/comprobaringresos/comprobaringresos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comprobaringresos',
+  templateUrl: './comprobaringresos.component.html',
+  styleUrls: ['./comprobaringresos.component.scss']
+})
+export class ComprobaringresosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
